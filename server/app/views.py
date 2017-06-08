6cd3c907c955e3bfccd74cfde4126fbb850a6263
Tu Tran <tu.tran@temple.edu>
2017-06-08 16:44:20 -0400

    add todo.txt to gitignore. split companies function to different smaller functions

--- server/app/views.py
+++ server/app/views.py
@@ -13,41 +13,51 @@ from app.models import Company, Founder
 companies_blueprint = Blueprint('companies', __name__)
 
 
+def get_all_companies():
+    pass
+
+
+def create_company():
+    # If data is empty or there is no field
+    if not (request.json and 'name' in request.json):
+        abort(400)
+
+    company = Company(
+        name=request.json['name'],
+        bio=request.json['bio'],
+        website=request.json['website'],
+    )
+    company.save()
+
+    # if user sends founder list
+    if 'founders' in request.json:
+        for founder in request.json['founders']:
+            Founder(
+                company_id=company.id,
+                email=founder.get('email', ''),
+                name=founder.get('name', ''),
+                role=founder.get('role', '')
+            ).save()
+
+    data = {
+        'status': 'success',
+        'message': 'new company created!',
+        'id': company.id
+    }
+    return make_response(jsonify(data)), 201
+
+
 @companies_blueprint.route('/companies', methods=['GET', 'POST'])
 def companies():
     """GET to retrieve all the companies
     POST to create a new company
     """
     if request.method == 'GET':
-        pass
+        return get_all_companies()
     elif request.method == 'POST':
-        # If data is empty or there is no field
-        if not (request.json and 'name' in request.json):
-            abort(400)
-
-        company = Company(
-            name=request.json['name'],
-            bio=request.json['bio'],
-            website=request.json['website'],
-        )
-        company.save()
-
-        # if user sends founder list
-        if 'founders' in request.json:
-            for founder in request.json['founders']:
-                Founder(
-                    company_id=company.id,
-                    email=founder.get('email', ''),
-                    name=founder.get('name', ''),
-                    role=founder.get('role', '')
-                ).save()
-
-        data = {
-            'status': 'success',
-            'message': 'new company created!',
-            'id': company.id
-        }
-        return make_response(jsonify(data)), 201
+        return create_company()
+    else:
+        return 405
 
 
 @companies_blueprint.route('/companies/<company_id>', methods=['GET'])
