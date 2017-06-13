9ceb1b2928deddc9a16e4a6ee71b9b5976092deb
Tu Tran <tu.tran@temple.edu>
2017-06-13 12:13:07 -0400

    Implemented GET for all the metrics + unit tests

--- server/app/apis/kpi.py
+++ server/app/apis/kpi.py
@@ -58,3 +58,87 @@ def post_company(company_id):
     response_data['message'] = 'metrics added'
 
     return jsonify(response_data), 200
+
+
+@kpi_blueprint.route('/companies/<int:company_id>/sales', methods=['GET'])
+def get_sales(company_id):
+    company = Company.query.get(company_id)
+    if not company:
+        return jsonify({
+            'status': 'failure',
+            'message': 'company not found'
+        }), 404
+
+    total_weeks = Sale.query.filter_by(company_id=company_id).count()
+    sales = Sale.query.filter_by(company_id=company_id).order_by(Sale.week).all()
+
+    return jsonify({
+        'weeks': total_weeks,
+        'data': list(map(
+            lambda sale: sale.value,
+            sales
+        ))
+    })
+
+
+@kpi_blueprint.route('/companies/<int:company_id>/customers', methods=['GET'])
+def get_customers(company_id):
+    company = Company.query.get(company_id)
+    if not company:
+        return jsonify({
+            'status': 'failure',
+            'message': 'company not found'
+        }), 404
+
+    total_weeks = Customer.query.filter_by(company_id=company_id).count()
+    customers = Customer.query.filter_by(company_id=company_id).order_by(Customer.week)
+
+    return jsonify({
+        'weeks': total_weeks,
+        'data': list(map(
+            lambda customer: customer.value,
+            customers
+        ))
+    })
+
+
+@kpi_blueprint.route('/companies/<int:company_id>/traffic', methods=['GET'])
+def get_traffic(company_id):
+    company = Company.query.get(company_id)
+    if not company:
+        return jsonify({
+            'status': 'failure',
+            'message': 'company not found'
+        }), 404
+
+    total_weeks = Traffic.query.filter_by(company_id=company_id).count()
+    traffic = Traffic.query.filter_by(company_id=company_id).order_by(Traffic.week)
+
+    return jsonify({
+        'weeks': total_weeks,
+        'data': list(map(
+            lambda traff: traff.value,
+            traffic
+        ))
+    })
+
+
+@kpi_blueprint.route('/companies/<int:company_id>/emails', methods=['GET'])
+def get_emails(company_id):
+    company = Company.query.get(company_id)
+    if not company:
+        return jsonify({
+            'status': 'failure',
+            'message': 'company not found'
+        }), 404
+
+    total_weeks = Email.query.filter_by(company_id=company_id).count()
+    emails = Email.query.filter_by(company_id=company_id).order_by(Email.week)
+
+    return jsonify({
+        'weeks': total_weeks,
+        'data': list(map(
+            lambda email: email.value,
+            emails
+        ))
+    })
